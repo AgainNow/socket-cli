d6159b5bc1d28d03b5b4f374e263653af5e0ba99
AgainNow <1143224186@qq.com>
2025-07-05 17:10:31 +0800

    更新说明文档

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # socket-cli
 socket客户端开发模板，基于系统调用、muduo等。
+
+## 需求
+
+1. 基于系统调用实现经典socket客户端
+2. 基于muduo实现经典socket客户端
+
+## 协议
+
+1. UDP
+2. TCP
+
+## 客户端设计
+
+1. 单线程单次请求 Simple Client
+2. 单线程循环请求 Iterative Client
+3. 多线程并发请求 Concurrent Client
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     // SimpleClient cli{};
     IterativeClient cli{};
     
-    // cli.udp("127.0.0.1", 1312);
-    cli.tcp("127.0.0.1", 1313);
+    cli.udp("127.0.0.1", 1312);
+    // cli.tcp("127.0.0.1", 1313);
     return 0;
 }
